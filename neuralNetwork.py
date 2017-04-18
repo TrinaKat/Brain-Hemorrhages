c51188f775886f9cf8c46c64c18b7618e8ded36f
Karen Li <karenli.614@gmail.com>
2017-04-18 02:23:28 -0700

    Start implementing CSV file reading

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -3,7 +3,22 @@ Implementation of deep neural network to predict brain hemorrhaging for stroke p
 """
 
 import numpy as np
-import tensorflow as tf 
+import tensorflow as tf
+
+# TODO: read in data (CSV file)
+filename_queue = tf.train.string_input_producer(["../trainingData/train_data_sm.csv"])
+
+reader = tf.TextLineReader()
+# read a single line from the file
+key, value = reader.read(filename_queue)
+with tf.Session() as sess:
+	# start populating the filename queue
+	coord = tf.train.Coordinator()
+	threads = tf.train.start_queue_runners(coord=coord)
+
+	# for i in range(40000):
+		# TODO: retrieve a single instance
+		
 
 # START INTERACTIVE SESSION
 
@@ -25,7 +40,17 @@ sess.run(tf.global_variables_initializer())
 
 # IMPLEMENT MODEL
 
-# currently, this is just a softmax regress model with a single linear layer
+# currently, this is just a softmax regression model with a single linear layer
 y = tf.matmul(X, W)
 
-# TODO: train the model
+# use the cross entropy as the error for the prediction
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+# TRAIN MODEL
+
+# use gradient descent with a learning rate of 0.5
+# running the "train_step" operation will run one step of gradient descent
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+# run the training step 1000 times
+# for _ in range(1000):
+	# TODO
