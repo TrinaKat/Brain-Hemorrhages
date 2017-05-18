d910585a056ddd82176e01092e08d8676fbd46e2
Karen Li <karenli.614@gmail.com>
2017-05-18 14:58:34 -0700

    Buggy evaluator

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -10,17 +10,18 @@ import tensorflow as tf
 # OBTAIN DATA 
 
 # read in training data (CSV file)
-filePath = "../trainingData/train_data_sm.csv"
-numRowsToSkip = 39000
+# 50000 rows, 623 columns
+filePath = "../trainingData/train_data_final2.csv"
+numRowsToSkip = 49000
 
 # get all the column indices except the first four columns and the last column
 columns = []
 for i in range(4, 622):
 	columns.append(i)
 
-# get a 40000 x 618 column array for all of the values (just 1000 x 618 for now)
+# get a 50000 x 618 column array for all of the values (just 1000 x 618 for now)
 trainingData = np.loadtxt(filePath, delimiter = ',', skiprows = numRowsToSkip, usecols = tuple(columns))	
-# get a 40000 x 1 column array for all of the results (boolean) (just 1000 x 1 for now)
+# get a 50000 x 1 column array for all of the results (boolean) (just 1000 x 1 for now)
 results = np.loadtxt(filePath, delimiter = ',', skiprows = numRowsToSkip, usecols = 622)
 
 # START INTERACTIVE SESSION
@@ -49,27 +50,25 @@ y = tf.matmul(X, W)
 
 # use the cross entropy as the error for the prediction
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+correct_prediction = tf.equal(y, y_)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # TRAIN AND EVALUATE MODEL
 
 # use gradient descent with a learning rate of 0.5
-# running the "train_step" operation will run one step of gradient descent
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 # run the training step on training set (800 samples)
 for i in range(0, 800):
+	# run one step of gradient descent
 	sess.run(train_step, feed_dict={X: np.reshape(trainingData[i], (1, 618)), y_: np.reshape(results[i], (1, 1))})
-	
-# run the evaluation on the test set (200 samples)
-numCorrect = 0
-# return whether or not y (prediction) equal y_  (actual result) (element-wise)
-correct_prediction = tf.equal(y, y_)
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-for i in range(800, 1000):
-	result = sess.run(accuracy, feed_dict={X: np.reshape(trainingData[i], (1, 618)), y_: np.reshape(results[i], (1, 1))})
-	# model predicted hemorrhage correctly
-	# TODO: check if I am testing this correctly
-	if (accuracy is not None):
-		numCorrect += 1
-	# print(accuracy.eval(feed_dict={X: np.reshape(trainingData[i], (1, 618)), y_: np.reshape(results[i], (1, 1))}))
+	# check to see how accurate the model is so far
+	if i % 50 == 0:
+		train_accuracy = accuracy.eval(feed_dict={X: np.reshape(trainingData[i], (1, 618)), y_: np.reshape(results[i], (1, 1))})
+		print("Training step %d: training accuracy %d"%(i, train_accuracy))
 
-print "Accuracy of predictions on test set: {}%".format(numCorrect/200 * 100)
+
+# run the evaluation on the test set (200 samples)
+for i in range(800, 1000):
+	test_accuracy = accuracy.eval(feed_dict={X: np.reshape(trainingData[i], (1, 618)), y_: np.reshape(results[i], (1, 1))})
+	# check the test result accuracy 
+	print("Testing step %d: testing accuracy %d"%(i, test_accuracy))
