eea964870198fadb9d14af1ac0d762758b6e8471
Karen Li <karenli.614@gmail.com>
2017-05-31 09:43:44 -0700

    Finish implementation of trees and random forests

--- skRandomForest.py
+++ skRandomForest.py
@@ -1,5 +1,5 @@
 """
-Implementation of SVM (support vector machine) to predict brain hemorrhaging for stroke patients
+Implementation of decision trees and random forests to predict brain hemorrhaging for stroke patients
 """
 
 # IMPORT REQUIRED LIBRARIES
@@ -7,12 +7,16 @@ Implementation of SVM (support vector machine) to predict brain hemorrhaging for
 import numpy as np
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import (RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier)
+from sklearn.tree import DecisionTreeClassifier
+
 
 # SET PARAMETERS
 
 numTrainingExamples = 30000
 numTestingExamples = 20000
 totalNumExamples = numTrainingExamples + numTestingExamples
+n_estimators = 30  # number of trees in the forest
 
 # OBTAIN DATA
 
@@ -37,5 +41,18 @@ results = np.loadtxt(filePath, delimiter = ',', skiprows = numRowsToSkip, usecol
 
 # TRAIN THE MODELS
 
+# accuracy: decision trees (92%), random forest (95%), extra-trees (96%), AdaBoost with decision trees (92%)
+models = [DecisionTreeClassifier(max_depth=None), RandomForestClassifier(n_estimators=n_estimators), ExtraTreesClassifier(n_estimators=n_estimators), AdaBoostClassifier(DecisionTreeClassifier(max_depth=3),n_estimators=n_estimators)]
+
 # randomly split the data into training set and test set (40% testing)
 X_train, X_test, y_train, y_test = train_test_split(trainingData_scaled, results, test_size=0.4, random_state=0)
+
+# train and evaluate all of the models
+for model in models:
+	# train the model
+	model.fit(X_train, y_train)
+
+	# evaluate the trained model on the test set
+	testAccuracy = model.score(X_test, y_test)
+
+	print("Final results for '%s': testing accuracy of %f%%"%(model, testAccuracy * 100))
