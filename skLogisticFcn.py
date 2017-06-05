332bc554b30a740e5f78d3908bd1f166eeb76b2c
Katrina Wijaya <katrina@thewijayas.com>
2017-06-05 04:48:26 -0700

    Preliminary scikit-learn version of LogReg

---
+++ skLogisticFcn.py
@@ -0,0 +1,85 @@
+# #!/usr/bin/python
+
+"""
+=========================================================
+Logistic function
+=========================================================
+
+Shown in the plot is how the logistic regression would, in this
+synthetic dataset, classify values as either 0 or 1,
+i.e. class one or two, using the logistic curve.
+
+"""
+
+"""
+Implementation of SVM (support vector machine) to predict brain hemorrhaging for stroke patients
+"""
+
+# IMPORT REQUIRED LIBRARIES
+
+import numpy as np
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+from sklearn import linear_model
+
+# SET PARAMETERS
+
+numTrainingExamples = 3000#0
+numTestingExamples = 2000#0
+totalNumExamples = numTrainingExamples + numTestingExamples
+
+# OBTAIN DATA
+
+# read in training data (CSV file)
+# 50000 rows, 623 columns
+filePath = "../trainingData/train_data_final2.csv"
+numRowsToSkip = 50000 - totalNumExamples
+
+# get all the column indices except the first four columns and the last column
+columns = []
+for i in range(4, 622):
+	columns.append(i)
+
+# get a 50000 x 618 column array for all of the values (just 1000 x 618 for now)
+trainingData = np.loadtxt(filePath, delimiter = ',', skiprows = numRowsToSkip, usecols = tuple(columns))
+# since SVMs aren't scale invariant, need to scale data
+# standard normally distributed data: Gaussian with zero mean and unit variance
+trainingData_scaled = preprocessing.scale(trainingData)
+
+# get a 50000 x 1 column array for all of the results (boolean) (just 1000 x 1 for now)
+results = np.loadtxt(filePath, delimiter = ',', skiprows = numRowsToSkip, usecols = (622,))
+
+# TRAIN THE MODELS
+
+# randomly split the data into training set and test set (40% testing)
+X_train, X_test, y_train, y_test = train_test_split(trainingData_scaled, results, test_size=0.4, random_state=0)
+
+# Use varying losses for linear classifiers with SGD training
+# Accuracy
+# hinge loss: 
+# logistic regression: 
+# linear loss with perceptron: 
+# smooth loss with outlier tolerance:
+# squared hinge loss w/ quadtractic penalization:
+
+# Run the classifier
+clf = linear_model.LogisticRegression() #83.5
+# Fit the model according to the given training data
+clf.fit(X_train, y_train)
+
+# evaluate the trained model on the test set
+# Returns the mean accuracy on the given test data and labels
+testAccuracy = clf.score(X_test, y_test)
+
+print("Final results for '%s': testing accuracy of %f%%"%(clf, testAccuracy * 100))
+
+
+# run the classifier
+# clf = linear_model.SGDClassifier() # default hinge loss = linear SVM #63
+# clf = linear_model.SGDClassifier(loss='log') # logistic regression is a probalistic classifier #78.5
+# clf = linear_model.SGDClassifier(loss='perceptron') #linear loss used by perceptron algorithm	#54
+# clf = linear_model.SGDClassifier(loss='modified_huber') #smooth loss with outlier tolerance #81
+# clf = linear_model.SGDClassifier(loss='squared_hinge') # like hinge which is linear SVM but quadractically penalized #74.5
+
+
